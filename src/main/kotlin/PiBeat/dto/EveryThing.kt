5bd424a01008b3d5ee65e4de97383f69e40b3350
Ngo Xuan Bach <thanbaiks@gmail.com>
2019-10-24 22:21:33 +0700

    Change default Port and Fix snapshot

--- src/main/kotlin/PiBeat/dto/EveryThing.kt
+++ src/main/kotlin/PiBeat/dto/EveryThing.kt
@@ -2,7 +2,7 @@ package PiBeat.dto
 import com.fasterxml.jackson.annotation.JsonIgnore
 import oshi.SystemInfo
 
-class EveryThing(@JsonIgnore val si: SystemInfo) {
-    val sysInfo get() = SysInfo(si)
-    val storage get() = Storage(si)
+class EveryThing(si: SystemInfo) {
+    val sysInfo = SysInfo(si)
+    val storage = Storage(si)
 }
