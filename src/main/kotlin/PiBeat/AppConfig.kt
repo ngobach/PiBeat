d1130d59c7c1d066043b97541949b88cf26dfc31
Ngo Xuan Bach <thanbaiks@gmail.com>
2019-10-26 16:32:20 +0700

    feat: Migrate to LoadPolicy = MERGE

--- src/main/kotlin/PiBeat/AppConfig.kt
+++ src/main/kotlin/PiBeat/AppConfig.kt
@@ -5,18 +5,16 @@ import org.aeonbits.owner.Config
 import org.aeonbits.owner.ConfigFactory
 
 @Config.Sources("file:app.properties", "classpath:app.properties")
+@Config.LoadPolicy(org.aeonbits.owner.Config.LoadType.MERGE)
 @JsonIgnoreProperties("mbeanInfo")
 interface AppConfig : Config {
     @get:Config.Key("server.port")
-    @get:Config.DefaultValue("8888")
     val port: Int
 
     @get:Config.Key("snapshot.interval")
-    @get:Config.DefaultValue("5000")
     val snapshotInterval: Long
 
     @get:Config.Key("json.pretty")
-    @get:Config.DefaultValue("false")
     val jsonPretty: Boolean
 }
 
