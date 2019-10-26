9ad03ad786472ad6a25ec1b449e7c7a692c36ad1
Ngo Xuan Bach <thanbaiks@gmail.com>
2019-10-26 16:47:10 +0700

    feat: Add compute metrics

--- src/main/kotlin/PiBeat/dto/EveryThing.kt
+++ src/main/kotlin/PiBeat/dto/EveryThing.kt
@@ -4,6 +4,7 @@ import oshi.SystemInfo
 class EveryThing(si: SystemInfo) {
     val sysInfo = SysInfo(si)
     val storage = Storage(si)
+    val compute = Compute(si)
     val memory = Memory(si)
     val network = Network(si)
 }
