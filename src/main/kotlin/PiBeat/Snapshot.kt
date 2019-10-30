3a4bef8f57a6b52a80d1c4e98defd98ee1c3eb33
Ngô Xuân Bách <ngo.xuan.bach@linecorp.com>
2019-10-30 13:03:55 +0700

    feat: Cleanup Snapshot

--- src/main/kotlin/PiBeat/Snapshot.kt
+++ src/main/kotlin/PiBeat/Snapshot.kt
@@ -1,22 +1,24 @@
 package PiBeat
 
 import PiBeat.dto.EveryThing
-import org.slf4j.LoggerFactory
 import oshi.SystemInfo
 import java.time.Instant
 
 object Snapshot : Runnable {
-    private val logger = LoggerFactory.getLogger(javaClass)
     var current: EveryThing? = null
         private set
     var timestamp: Long = 0
         private set
+    private val si = SystemInfo()
+
+    private fun collect() {
+        current = EveryThing(si)
+        timestamp = Instant.now().epochSecond
+    }
 
     override fun run() {
-        val si = SystemInfo()
         kotlin.concurrent.fixedRateTimer(period = Config.snapshotInterval) {
-            current = EveryThing(si)
-            timestamp = Instant.now().epochSecond
+            collect()
         }
     }
 }
